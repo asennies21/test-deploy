b8b15ab96bbd520d6ffd37ee6810ab393506ef0d
asennies21 <asenn@ies21.edu.ar>
2026-06-15 23:36:13 -0300

    coneccion BD

--- index.mjs
+++ index.mjs
@@ -1,11 +1,21 @@
 import express from 'express'
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  user: process.env.DB_USER || 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'prueba',
+  password: process.env.DB_PASSWORD || 'admin',
+  port: process.env.DB_PORT || 5432,
+});
 
 const puerto = 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Despliegue exitoso');
+app.get('/', async (req, res) => {
+  const resultado = await pool.query('SELECT *- from test')
+  res.send(resultado.rows);
 })
 app.listen(puerto, () => {
   console.log(`Servidor escuchando en el puerto ${puerto}`);

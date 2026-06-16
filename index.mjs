0dd6a4ef2f44db68610cba5c13b494e95e2df766
asennies21 <asenn@ies21.edu.ar>
2026-06-15 23:12:14 -0300

    Init

---
+++ index.mjs
@@ -0,0 +1,12 @@
+import epxress from 'express'
+
+const puerto = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Despliegue exitoso');
+})
+app.listen(puerto, () => {
+  console.log(`Servidor escuchando en el puerto ${puerto}`);
+});
